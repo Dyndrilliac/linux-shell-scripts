7c8c12943e7c92271b5be3a9c3599f82e7cc6bfa
Matthew Boyette <Dyndrilliac@gmail.com>
2016-11-04 19:38:13 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,17 +4,12 @@
 # Author: Dyndrilliac (Matthew Boyette) #
 # Date:   11/21/2012                    #
 #########################################
-# Make sure the user is running the script as root.
-if [ "`id -u`" -ne 0 ]; then
-	echo "This script must be run as root! Exiting..."
-	exit 1
-fi
 # Clean up installed software and pull updates.
-apt-get autoremove
-apt-get autoclean
-apt-get check
-apt-get -f install
-apt-get update
-apt-get dist-upgrade
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get check
+sudo apt-get -f install
+sudo apt-get update
+sudo apt-get dist-upgrade
 # Purge packages no longer being used.
-dpkg -l | grep '^rc' | awk '{print $2}' | xargs dpkg --purge
+sudo dpkg -l | sudo grep '^rc' | sudo awk '{print $2}' | sudo xargs dpkg --purge
